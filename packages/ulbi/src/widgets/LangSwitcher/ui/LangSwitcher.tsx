0760c7ba45d2dd79f4c404d8ab35cca5fe69cc02
Dzmitriy Dovguchev <ddovguchev@gmail.com>
2025-11-18 13:20:21 +0300

    cources 15 i18n Интернационализация. Define plugin. Плагин для переводов
    * добавили i18n
    * добавили получение build mode из webpack

---
+++ packages/ulbi/src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -0,0 +1,27 @@
+import { useTranslation } from "react-i18next";
+import i18n from "shared/config/i18n/i18n.ts";
+import { classNames } from "shared/lib/classNames/classNames.ts";
+import { Button, ThemeButton } from "shared/ui/Button/Button.tsx";
+
+interface ILangSwitcherProps {
+    className?: string;
+}
+
+export const LangSwitcher = ({ className }: ILangSwitcherProps) => {
+    const { t } = useTranslation("common");
+
+    const toggle = () => {
+        const currentLang = i18n.language;
+        const languages = ['ru', 'en', 'be'];
+        const currentIndex = languages.indexOf(currentLang);
+        const nextIndex = (currentIndex + 1) % languages.length;
+        void i18n.changeLanguage(languages[nextIndex]);
+    };
+
+    return (
+        <Button theme={ThemeButton.CLEAR} onClick={toggle} className={classNames("", {}, [className])}>
+            {t("languageName")}
+        </Button>
+    );
+};
+
